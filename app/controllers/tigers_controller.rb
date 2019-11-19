fc224a9c14edd4e2accabf95aa3a9d0473d093f2
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-19 13:47:26 +1100

    add tiger controllers

--- app/controllers/tigers_controller.rb
+++ app/controllers/tigers_controller.rb
@@ -1,13 +1,13 @@
 class TigersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-  before_action :find_tiger, only: %i[show]
+  before_action :find_tiger, only: %i[show edit update destroy]
 
   def index
     @tigers = Tiger.all
   end
 
   def show
-    # @tiger = Tiger.find(params[:id])
+    # @tiger in find_tiger
   end
 
   def new
@@ -15,17 +15,34 @@ class TigersController < ApplicationController
   end
 
   def create
+    @tiger = Tiger.new(tiger_params)
+    @tiger.user = current_user
+
+    if @tiger.save
+      redirect_to @tiger, notice: 'Tiger Was Successfuly Created'
+    else
+      render :new
+    end
   end
 
   def edit
+    # @tiger in find_tiger
   end
 
   def update
+    # @tiger in find_tiger
+    if @tiger.update(tiger_params)
+      redirect_to @tiger, notice: 'Tiger Was Successfuly Updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    # @tiger in find_tiger
+    @tiger.destroy
+    redirect_to root_path, notice: 'Tiger Was Successfuly Deleted'
   end
-
 
   private
 
@@ -34,6 +51,6 @@ class TigersController < ApplicationController
   end
 
   def tiger_params
-    params.require(:tiger).permit()
+    params.require(:tiger).permit(:age, :species, :price, :location)
   end
 end
