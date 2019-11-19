64665404230209217831e71265bcd7aebfb3f0db
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-19 15:35:43 +1100

    code a bit of controller (not done yet)

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,41 @@
+class BookingsController < ApplicationController
+  before_action :find_booking, only: %i[create destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    # @booking from find_booking
+    # find the tiger
+    @tiger = Tiger.find(params[:id])
+    @booking = Booking.new(booking_params)
+    # set the booking.tiger to the tiger object
+    @booking.tiger = @tiger
+    # user = current_user whoever makes the booking
+    @booking.user = current_user
+
+    if @tiger.save
+      redirect_to @booking, notice: 'Booking was successful'
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:booking_start, :booking_finish, :user, :tiger)
+  end
+end
