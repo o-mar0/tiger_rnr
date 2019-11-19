a3b40db08d0a746ca021957b896bd6fd1bd18bbf
Vincent Tso <vincent11102@hotmail.com>
2019-11-20 10:41:15 +1100

    edit bookings index page, add save to total price method in bookings model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tigers
+  has_many :tigers, dependent: :destroy
   has_many :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
