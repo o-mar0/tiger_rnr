94fffcfa35b7b5f31e7e7d04b31923ac4211ba98
Vincent Tso <vincent11102@hotmail.com>
2019-11-20 23:13:39 +1100

    change landing page and show page, edit navbar functionality and form style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 import "bootstrap";
 import { scrollNavbar } from '../components/navbar';
+import 'select2/dist/css/select2.css';
 
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
 scrollNavbar();
