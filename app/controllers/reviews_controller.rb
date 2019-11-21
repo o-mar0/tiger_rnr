0e48a27873138574837323b41ac254b884e1ba20
Vincent Tso <vincent11102@hotmail.com>
2019-11-22 10:48:40 +1100

    add review to show page and edit navbar, edit user model associations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,8 +3,10 @@ class ReviewsController < ApplicationController
     @tiger = Tiger.find(params[:tiger_id])
     @review = Review.new(review_params)
     @review.tiger = @tiger
+    @review.user = current_user
 
     if @review.save
+      redirect_to tiger_path(@tiger), notice: 'Review was created'
     else
       render 'tigers/show'
     end
@@ -13,6 +15,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :author, :rating, :tiger)
+    params.require(:review).permit(:content, :user, :rating, :tiger)
   end
 end
