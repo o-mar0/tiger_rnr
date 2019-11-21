7aebaa9e496201544ff36dd01d72911336ab2ae0
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-21 11:35:27 +1100

    add search

--- app/models/tiger.rb
+++ app/models/tiger.rb
@@ -6,4 +6,14 @@ class Tiger < ApplicationRecord
 
   validates :name, :age, :location, :species, :price, :user_id, :photo, presence: true
   validates :age, :price, numericality: { only_integer: true }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_location,
+                  against: %i[name location],
+                  associated_against: {
+                    user: %i[username]
+                  },
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
