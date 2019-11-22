8e532edeec833571695d81e6c3d848417b439797
Wadeebha Padmakumara <wadeebha@gmail.com>
2019-11-22 11:12:00 +1100

    Footer fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import 'select2/dist/css/select2.css';
 import "flatpickr/dist/flatpickr.min.css";
 import "flatpickr/dist/themes/material_orange.css";
 import "../plugins/flatpickr";
-// import "bootstrap";
+import "bootstrap";
 
 import { scrollNavbar } from '../components/navbar';
 import { initSelect2 } from '../components/init_select2';
